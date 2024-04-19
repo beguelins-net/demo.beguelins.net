54b215b476c31994bdd9262476e944dcf3ea4e78
Thomas Chabaud <dev@chabs.name>
2024-04-19 12:02:42 +0000

    chore: cleanup, bump to next.js 14 and outstatic advanced blog template.

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,11 +1,14 @@
+export const metadata = {
+  title: "Béguelins-Béguelines",
+};
 export default function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
